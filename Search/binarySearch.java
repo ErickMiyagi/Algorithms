b3342c355834dd40cf354804279357f2fcb70254
Erick <erick_miyagi34231@hotmail.com>
2024-04-26 09:37:44 -0300

    feat(add): first algorithm: Binary Search

---
+++ Search/binarySearch.java
@@ -0,0 +1,25 @@
+package Search;
+
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] list = {9,1,2,3,6,8};
+        System.out.println(searchBinary(list, 6));
+    }
+    public static Integer searchBinary(int[] list, int number){
+        int low = 0;
+        int high = list.length - 1; // 4
+        
+        while(low <= high){
+        int mid = (low + high) / 2; // 2
+        int guess = list[mid]; //list[2] = 2
+        if(guess == number){
+            return mid; //false
+        } else if(guess > number){
+            high = mid - 1; //false
+        } else {
+            low = mid + 1; //true: low = 3
+        }
+    }
+    return null;
+    }
+}
